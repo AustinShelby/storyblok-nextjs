6c3aee46a1108c53f4567e65615a0279e547e887
Austin Shelby <helloaustinshelby@gmail.com>
2023-11-06 17:55:38 +0200

    add tour list page

--- src/app/tours/page.tsx
+++ src/app/tours/page.tsx
@@ -1,5 +1,34 @@
-const ToursPage = () => {
-  return <div>Tours Page</div>;
+import { getStoryblokApi, StoryblokStory } from "@storyblok/react/rsc";
+import { RecommendedTour } from "@/components/RecommendedTour";
+
+const fetchToursPage = async () => {
+  const client = getStoryblokApi();
+  const response = await client.getStory(`tours`, {
+    version: "draft",
+  });
+  return response.data.story;
 };
 
+const fetchAllTours = async () => {
+  const client = getStoryblokApi();
+  const response = await client.getStories({
+    content_type: "tour",
+    version: "draft",
+  });
+  return response.data.stories;
+};
+
+const ToursPage = async () => {
+  const story = await fetchToursPage();
+  const tours = await fetchAllTours();
+
+  return (
+    <div>
+      <StoryblokStory story={story} />
+      {tours.map((tour) => (
+        <RecommendedTour story={tour} key={tour.content._uid} />
+      ))}
+    </div>
+  );
+};
 export default ToursPage;
