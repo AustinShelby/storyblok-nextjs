0a9f27e7f50f97489f217189dff10a34b8e35d42
Austin Shelby <helloaustinshelby@gmail.com>
2023-11-13 22:44:28 +0200

    resolve relations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,12 @@ const fetchHomePage = async () => {
 
 const HomePage = async () => {
   const story = await fetchHomePage();
-  return <StoryblokStory story={story} />;
+  return (
+    <StoryblokStory
+      bridgeOptions={{ resolveRelations: ["recommended_tours.tours"] }}
+      story={story}
+    />
+  );
 };
 
 export default HomePage;
